c679ebbe904f92c8fd12747bd3b6c37ea187ed0b
Chandrasekar Mohan <chandrasekar.mohan@gmail.com>
2023-09-18 23:23:33 -0700

    Added indexDB getDB and putDB logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,39 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  
+  const jateDb = await openDB("jate", 1);
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction("jate", "readwrite");
+  // Open up the desired object store.
+	const store = tx.objectStore("jate");
+  // Get confirmation of the request.
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('putDb - data saved successfully ', result);
+
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB("jate", 1);
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction("jate", "readonly");
+  // Open up the desired object store.
+	const store = tx.objectStore("jate");
+  // Get confirmation of the request.
+  const request = store.get(1);
+
+	// Get confirmation of the request.
+	const result = await request;
+
+  result
+		? console.log("getDB - data retrieved from the database", result.value)
+		: console.log("getDB - data not found in the database");
+  
+  return result?.value;
+}
+
 
 initdb();
