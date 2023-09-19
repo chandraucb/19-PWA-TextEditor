15da667318e8b56abccdf49dbcb12fb0c666b346
Chandrasekar Mohan <chandrasekar.mohan@gmail.com>
2023-09-18 23:09:02 -0700

    Updated webpack config to include plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,11 +18,62 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // Webpack plugin that generates our html file and injects our bundles.
+			new HtmlWebpackPlugin({
+				template: "./index.html",
+				title: "Just Another Text Editor",
+			}),
+
+			// Injects our custom service worker.
+			new InjectManifest({
+				swSrc: "./src-sw.js",
+				swDest: "src-sw.js",
+			}),
+
+			// Creates a manifest.json file.
+			new WebpackPwaManifest({
+				fingerprints: false,
+				inject: true,
+				name: "Just Another Text Editor",
+				short_name: "J.A.T.E.",
+				description: "Create notes with or without an internet connection!",
+				background_color: "#272822",
+				theme_color: "#272822",
+				start_url: "./",
+				publicPath: "./",
+				icons: [
+					{
+						src: path.resolve("src/images/logo.png"),
+						sizes: [96, 128, 192, 256, 384, 512],
+						destination: path.join("assets", "icons"),
+					},
+				],
+			}),
       
     ],
 
     module: {
       rules: [
+        // Add CSS loaders to webpack.
+				{
+					test: /\.css$/i,
+					use: ["style-loader", "css-loader"],
+				},
+				// Add babel to webpack in order to use ES6.
+				{
+					test: /\.m?js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-env"],
+							plugins: [
+								"@babel/plugin-proposal-object-rest-spread",
+								"@babel/transform-runtime",
+							],
+						},
+					},
+				},
         
       ],
     },
